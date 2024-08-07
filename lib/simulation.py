be0c802b2c4d02de886ea5dafd45d543af8f9063
el19ps <hipralish@gmail.com>
2024-08-07 15:34:49 +0100

    [Unstable]
    Unstable update -> For saving work on the 7th August 2024. This code is currently broken but verbose redudnant np.array calling has been removed.

--- lib/simulation.py
+++ lib/simulation.py
@@ -260,17 +260,12 @@ class SimulationProcessor:
         
         print("About to filter all coordiantes within plot_toolpath_animation\n")
         filtered_e_coords = filter_close_coordinates(common_e_coords_np)
-        #print(f"Filtered e coordinates with length {len(filtered_e_coords)} and type {type(filtered_e_coords)}")
-
         filtered_travel_coords = filter_close_coordinates(travel_coords)
-        #print(f"Filtered travel coordinates with length {len(filtered_travel_coords)} and type {type(filtered_travel_coords)}")
-
         filtered_coords = filter_close_coordinates(coords)
-        #print(f"Filtered coordinates with length {len(filtered_coords)} and type {type(filtered_coords)}")
 
-        self.common_e_coords_np = np.array(filtered_e_coords)
-        self.travel_coords_np = np.array(filtered_travel_coords)
-        self.coords_np = np.array(filtered_coords)
+        self.common_e_coords_np = filtered_e_coords
+        self.travel_coords_np = filtered_travel_coords
+        self.coords_np = filtered_coords
 
         self.segments = [self.split_into_segments_new(self.coords_np)]
         print(f"The length of segments with the new approach is {len(self.segments)}")
@@ -447,25 +442,16 @@ def filter_close_coordinates(coords_np, threshold=0.1):
     """Filter out coordinates that are too close to each other based on the threshold."""
     if coords_np.size == 0:
         return coords_np
-    #print(f"You have input coordinates of length {len(coords_np)}")
-    #print(f"You have input coordinates of type {type(coords_np)}")
-
-    # Ensure the array is of type float for distance calculations
+    print(f"You have input coordinates of length {len(coords_np)}")
     coords_np = coords_np.astype(float)
-    #print(f"You have input coordinates of type {type(coords_np)}")
-
-    # Initialize the list of filtered coordinates
+    
     filtered_coords = [coords_np[0]]
-    #print(f"initialised filtered coords list as {filtered_coords} with size {len(filtered_coords)} and type {type(filtered_coords)}\n")
-    #print("About to start for loop")
-
     for coord in coords_np[1:]:
         # Calculate Euclidean distance from the last filtered coordinate
         last_filtered_coord = filtered_coords[-1]
         distance = np.linalg.norm(coord - last_filtered_coord)
         if distance > threshold:
             filtered_coords.append(coord)
-    #print(f"Returning length of final set of filtered_coords {len(filtered_coords)}")
-    #print(len(filtered_coords))
+    print(f"Returning length of final set of filtered_coords {len(filtered_coords)}")
     filtered_coords_np = np.array(filtered_coords)
-    return np.array(filtered_coords_np)
+    return filtered_coords_np
